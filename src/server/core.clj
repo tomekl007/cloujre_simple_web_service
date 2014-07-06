7af1ba0966733d6c55edc7f6392a6f7e1056e15c
tomekl007 <tomekl007@gmail.com>
2014-07-06 18:14:35 +0200

    .

--- src/server/core.clj
+++ src/server/core.clj
@@ -1,11 +1,3 @@
-;---
-; Excerpted from "Seven Concurrency Models in Seven Weeks",
-; published by The Pragmatic Bookshelf.
-; Copyrights apply to this code. It may not be used to create training material, 
-; courses, books, articles, and the like. Contact us if you are in doubt.
-; We make no guarantees that this code is fit for any purpose. 
-; Visit http://www.pragmaticprogrammer.com/titles/pb7con for more book information.
-;---
 (ns server.core
   (:require [clojure.edn        :as    edn]
             [compojure.core     :refer :all]
@@ -15,7 +7,7 @@
 
 (def snippets (repeatedly promise))
 
-(future 
+(future
   (doseq [snippet (map deref snippets)]
     (println snippet)))
 
